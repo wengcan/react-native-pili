ca22a7c6b923a4c48aac3cb29c67fd4bc0dce98b
wengcan <wengcanvip@gmail.com>
2017-02-20 12:14:08 +0000

    chang index requirement

--- index.js
+++ index.js
@@ -2,8 +2,5 @@
  * Created by buhe on 16/4/28.
  */
 module.exports = {
-  Streaming: require('./Streaming'),
-  AudioStreaming: require('./AudioStreaming'),
-  Player: require('./Player'),
-  StreamingConst: require('./StreamingConst')
+  Player: require('./Player')
 };
